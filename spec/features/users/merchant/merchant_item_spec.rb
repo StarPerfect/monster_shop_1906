5725054aa69f2d5af9270e6833fc49673ce3b69a
Corina Allen <StarPerfect@gmail.com>
2019-09-11 11:34:05 -0600

    Clean up last few errors
    Co-authored-by: Scott Payton <133855435+wolfeJob@users.noreply.github.com>
    Co-authored-by: Kelly Sandeson <46202366+ksandy95@users.noreply.github.com>
    Co-authored-by: Santiago Caraveo <43061381+Santiag0C@users.noreply.github.com>

--- spec/features/users/merchant/merchant_item_spec.rb
+++ spec/features/users/merchant/merchant_item_spec.rb
@@ -1,26 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchant Admin can CRUD items' do
-  before(:each) do
-    @bike_city = Merchant.create(name: "Meg's Bike City", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-    @store_admin = User.create(name: "Billy Joel", address: "123 Billy Street", city: "Denver", state: "CO", zip: "80011", email:"billobill@gmail.com", password: "yours", role: 1,  merchant_id: @bike_city.id )
-    @user = User.create(name: "Scott Payton", address: "123 Scott Street", city: "Stapleton", state: "CO", zip: "80011", email:"bigfun@gmail.com", password: "mine", role: 0)
-    @tire = @bike_city.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-    @pedals = @bike_city.items.create(name: "Brass Knucks Pedals", description: "They'll make you feel like you're on speed!", price: 150, image: "https://content.competitivecyclist.com/images/items/900/SPP/SPP000A/POL.jpg", inventory: 5)
-    @brakes = @bike_city.items.create(name: "Disc Brakes", description: "They'll stop you on a dime!", price: 70, image: "https://www.excelsports.com/assets/gallery/110464-1.jpg", inventory: 12)
-  end
+  it 'merchant admin can deactivate an active item then reactivate' do
+    bike_city = Merchant.create(name: "Meg's Bike City", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    store_admin = User.create(name: "Billy Joel", address: "123 Billy Street", city: "Denver", state: "CO", zip: "80011", email:"billobillgmail.com", password: "yours", role: 1,  merchant_id: bike_city.id )
+    user = User.create(name: "Scott Payton", address: "123 Scott Street", city: "Stapleton", state: "CO", zip: "80011", email:"bigfungmail.com", password: "mine", role: 0)
+    pedals = bike_city.items.create(name: "Brass Knucks Pedals", description: "They'll make you feel like you're on speed!", price: 150, image: "https://content.competitivecyclist.com/images/items/900/SPP/SPP000A/POL.jpg", inventory: 5)
 
-  it 'merchant admin can deactivate an active item' do
     visit login_path
 
-    fill_in :email, with: 'billobill@gmail.com'
+    fill_in :email, with: 'billobillgmail.com'
     fill_in :password, with: 'yours'
 
     click_button 'Login'
 
     click_link 'Merchant Items'
 
-    within "#merchant-item-#{@pedals.id}" do
+    within "#merchant-item-#{pedals.id}" do
       click_link 'Deactivate'
 
       expect(page).to_not have_content(true)
@@ -30,21 +26,26 @@ RSpec.describe 'Merchant Admin can CRUD items' do
   end
 
   it 'merchant admin can activate an deactive item' do
+    bike_city = Merchant.create(name: "Meg's Bike City", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    store_admin = User.create(name: "Billy Joel", address: "123 Billy Street", city: "Denver", state: "CO", zip: "80011", email:"billobillgmail.com", password: "yours", role: 1,  merchant_id: bike_city.id )
+    user = User.create(name: "Scott Payton", address: "123 Scott Street", city: "Stapleton", state: "CO", zip: "80011", email:"bigfungmail.com", password: "mine", role: 0)
+    brakes = bike_city.items.create(name: "Disc Brakes", description: "They'll stop you on a dime!", price: 70, image: "https://www.excelsports.com/assets/gallery/110464-1.jpg", inventory: 12, active?: false)
+
     visit login_path
 
-    fill_in :email, with: 'billobill@gmail.com'
+    fill_in :email, with: 'billobillgmail.com'
     fill_in :password, with: 'yours'
 
     click_button 'Login'
 
     click_link 'Merchant Items'
 
-    within "#merchant-item-#{@brakes.id}" do
+    within "#merchant-item-#{brakes.id}" do
       click_link 'Activate'
-
-      expect(page).to_not have_content(true)
-      expect(page).to have_content(false)
-      expect(page).to have_link('Dectivate')
     end
+
+      expect(page).to_not have_content(false)
+      expect(page).to have_content(true)
+      expect(page).to have_link('Deactivate')
   end
 end
