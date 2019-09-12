366b827731876437d7e190a3bb5e96d4a2ad7cd6
Santiago Caraveo <santiagocbrvg@hotmail.com>
2019-09-11 18:15:31 -0700

    Create new method
    Co-Authored-By: Kelly Sandeson <46202366+ksandy95@users.noreply.github.com>
    Co-Authored-By: Scott Payton <133855435+wolfeJob@users.noreply.github.com>

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -1,11 +1,21 @@
 class Merchant::DashboardController < ApplicationController
-# require :merchant
+
+  def new
+    merchant = current_user.merchant.id
+    @merchant = Merchant.find(merchant)
+    @item = @merchant.items.new
+  end
 
   def show
     @user = current_user
     if @user.role == 1 || 2
       @merchant = Merchant.find(@user.merchant_id)
     end
+  end
+
+  def index
+    @user = current_user
+    @merchant = Merchant.find(@user.merchant_id)
   end
 
   def update
@@ -29,5 +39,49 @@ class Merchant::DashboardController < ApplicationController
       @item.update(active?: true)
     end
     redirect_to '/merchant/items'
+  end
+
+  def edit
+    @item = Item.find(params[:format])
+  end
+
+  def item_update
+    item = Item.find(params[:id])
+    if item_params[:price].to_i <= 0 || item_params[:inventory].to_i <= 0
+      flash[:error] = item.errors.full_messages.to_sentence
+      render :edit
+    else
+      item.update(item_params)
+      redirect_to '/merchant/items'
+    end
+  end
+
+  def create
+    if item_params[:price].to_i <= 0 || item_params[:inventory].to_i <= 0
+      item = Item.new(item_params)
+      flash[:error] = item.errors.full_messages.to_sentence
+      render :new
+    else
+      item = current_user.merchant.items.create(item_params)
+    end
+      if item.save
+        flash[:notice] = "Your new item has saved!"
+        redirect_to "/merchant/items"
+      else
+        flash[:error] = item.errors.full_messages.to_sentence
+        render :new
+      end
+  end
+
+  def destroy
+    Item.delete(Item.where(id: params[:format]))
+    flash[:notice] = "Our big hangry monster has eaten your item!"
+    redirect_to '/merchant/items'
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :description, :image, :inventory)
   end
 end
