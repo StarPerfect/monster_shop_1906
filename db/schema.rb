2466ee110dd45ee4e8660b337a1069f39304e12c
Corina Allen <StarPerfect@gmail.com>
2019-09-15 15:56:10 -0600

    Remove address details from Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190915153707) do
+ActiveRecord::Schema.define(version: 20190915215440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,10 +86,6 @@ ActiveRecord::Schema.define(version: 20190915153707) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "address"
-    t.string "city"
-    t.string "state"
-    t.string "zip"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
