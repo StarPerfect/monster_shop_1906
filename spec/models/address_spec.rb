4cffe1865f9415ee8ea06c6be2d302eb5807c018
Corina Allen <StarPerfect@gmail.com>
2019-09-15 15:42:03 -0600

    Move validations of address into Address model spec

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe Address, type: :model do
   describe 'relationships' do
     it { should belong_to :user }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :street }
+    it { should validate_presence_of :city }
+    it { should validate_presence_of :state }
+    it { should validate_presence_of :zip }
+  end
 end
