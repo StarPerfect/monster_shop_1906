b75edd51c92246abe0cbfb0c6763033f60976cda
Corina Allen <StarPerfect@gmail.com>
2019-09-17 13:19:28 -0600

    Add address and order relationships

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,7 +2,7 @@ class Address < ApplicationRecord
   validates_presence_of :nickname, :street, :city, :state, :zip
 
   belongs_to :user#, inverse_of: :addresses
-  belongs_to :order
+  has_many :order
 
   validates :user, presence: true
 end
