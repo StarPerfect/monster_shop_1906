adb47dc8d938f80648273cc73497901c606d5915
Corina Allen <StarPerfect@gmail.com>
2019-09-17 17:14:41 -0600

    Create index, edit and destroy actions for AddressesController

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,5 +1,19 @@
 class AddressesController < ApplicationController
   def index
     @user = User.find(params[:user_id])
+    @addresses = User.find(params[:user_id]).addresses
   end
+
+  def edit
+    @address = Address.find(params[:id])
+  end
+
+  def destroy
+  end
+
+  # private
+  # <%= link_to "Delete #{address.nickname} Address", delete_user_address(@user.id,address.id), method: :delete %>
+  # def address_params
+  #   params.require(:address).permit(:nickname, :street, :city, :state, :zip)
+  # end
 end
