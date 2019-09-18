facbe72fffbad696ba7ceb2e00fe05ca99dfea3c
Corina Allen <StarPerfect@gmail.com>
2019-09-18 17:37:02 -0600

    Add ActiveRecord method to check if address has any shipped orders

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,7 +2,11 @@ class Address < ApplicationRecord
   validates_presence_of :nickname, :street, :city, :state, :zip
 
   belongs_to :user#, inverse_of: :addresses
-  has_many :order
+  has_many :orders
 
   validates :user, presence: true
+
+  def has_shipped_orders?
+    self.orders.pluck(:status).any?{ |status| status == 'shipped' }
+  end
 end
